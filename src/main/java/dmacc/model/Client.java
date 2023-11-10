6d6246b5914040ea67cebfb4a5ef9a1dc8144d73
Jacey Espinosa <104535773+jaceyespinosa@users.noreply.github.com>
2023-11-09 18:49:06 -0600

    Initial object classes

---
+++ src/main/java/dmacc/model/Client.java
@@ -0,0 +1,67 @@
+package dmacc.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "client", schema = "doggiedaycare")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contactInfo;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Pet> pets;
+    
+    public Client() {
+    	super();
+    }
+    
+    public Client(Long id, String name, String contactInfo, List<Pet> pets) {
+    	this.id = id;
+    	this.name = name;
+    	this.contactInfo = contactInfo;
+    	this.pets = pets;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactInfo() {
+		return contactInfo;
+	}
+
+	public void setContactInfo(String contactInfo) {
+		this.contactInfo = contactInfo;
+	}
+
+	public List<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(List<Pet> pets) {
+		this.pets = pets;
+	}
+}
---
+++ src/main/java/dmacc/model/Pet.java
@@ -0,0 +1,93 @@
+/**
+ * @author Jacey Espinosa - JLESPINOSA
+ * CIS175 - FALL 2023
+ * Nov 11, 2023
+ */
+
+package dmacc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "pet", schema = "doggiedaycare")
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String breed;
+    private int age;
+    
+	@ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Client owner;
+	
+    public Pet() {
+    	super();
+    }
+    
+    public Pet(Long id, String name, String breed, int age) {
+    	this.id = id;
+    	this.name = name;
+    	this.breed = breed;
+    	this.age = age;
+    }
+    
+    public Pet(Long id, String name, String breed, int age, Client owner) {
+    	this.id = id;
+    	this.name = name;
+    	this.breed = breed;
+    	this.age = age;
+    	this.owner = owner;
+    }
+    
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Client getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Client owner) {
+		this.owner = owner;
+	}
+
+
+}
